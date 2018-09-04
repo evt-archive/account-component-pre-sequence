Senjai/account-component

[STEP] Deposit reservation

exercise/handle_reissued_deposit.rb
@@ -1,13 +1,16 @@
 require_relative './exercise_init'
 
 account_id = Identifier::UUID::Random.get
+deposit_id = Identifier::UUID::Random.get
 
 deposit = Messages::Commands::Deposit.new
+deposit.deposit_id = deposit_id
 deposit.account_id = account_id
 deposit.amount = 11
 deposit.time = '2000-01-01T11:11:11.000Z'
 
 command_stream_name = "account:command-#{account_id}"
+transaction_stream_name = "accountTransaction-#{deposit_id}"
 
 
 store = Store.build
@@ -24,6 +27,12 @@ puts "Account #{account_id} balance: $#{account.balance}"
 
 EventSource::Postgres::Read.(command_stream_name) do |message_data|
   Handlers::Commands.(message_data)
+end
+
+puts "Handling transaction commands"
+
+EventSource::Postgres::Read.(transaction_stream_name) do |message_data|
+  Handlers::Commands::Transactions.(message_data)
 end
 
 

test/automated/handle_commands/deposit/not_reserved.rb
@@ -1,0 +1,23 @@
+require_relative '../../../test_init'
+
+context "Handle Commands" do
+  context "Deposit" do
+    context "Not Reserved" do
+      deposit_id = Identifier::UUID::Random.get
+
+      deposit = Controls::Commands::Deposit.example(id: deposit_id)
+
+      transaction_stream_name = "accountTransaction-#{deposit_id}"
+
+      Messaging::Postgres::Write.(deposit, transaction_stream_name)
+
+      Handlers::Commands.(deposit)
+
+      event_data, * = EventSource::Postgres::Get.(transaction_stream_name, position: 1, batch_size: 1)
+
+      test "Deposit command is not written to transaction stream" do
+        assert(event_data.nil?)
+      end
+    end
+  end
+end

test/automated/handle_commands/deposit/reserved.rb
@@ -1,0 +1,41 @@
+require_relative '../../../test_init'
+
+context "Handle Commands" do
+  context "Deposit" do
+    context "Reserved" do
+      deposit_id = Identifier::UUID::Random.get
+
+      deposit = Controls::Commands::Deposit.example(id: deposit_id)
+
+      Handlers::Commands.(deposit)
+
+      transaction_stream_name = "accountTransaction-#{deposit_id}"
+
+      event_data, * = EventSource::Postgres::Get.(transaction_stream_name, position: 0, batch_size: 1)
+
+      written_message = Messaging::Message::Import.(event_data, deposit.class)
+
+      test "Deposit command is written to transaction stream" do
+        refute(written_message.nil?)
+      end
+
+      context "Attributes" do
+        test "deposit_id" do
+          assert(written_message.deposit_id == deposit.deposit_id)
+        end
+
+        test "account_id" do
+          assert(written_message.account_id == deposit.account_id)
+        end
+
+        test "amount" do
+          assert(written_message.amount == deposit.amount)
+        end
+
+        test "time" do
+          assert(written_message.time == deposit.time)
+        end
+      end
+    end
+  end
+end

evt-archive/account-component-pre-sequence

exercise/handle_reissued_deposit.rb
@@ -1,13 +1,16 @@
 require_relative './exercise_init'
 
 account_id = Identifier::UUID::Random.get
+deposit_id = Identifier::UUID::Random.get
 
 deposit = Messages::Commands::Deposit.new
+deposit.deposit_id = deposit_id
 deposit.account_id = account_id
 deposit.amount = 11
 deposit.time = '2000-01-01T11:11:11.000Z'
 
 command_stream_name = "account:command-#{account_id}"
+transaction_stream_name = "accountTransaction-#{deposit_id}"
 
 
 store = Store.build
@@ -24,6 +27,12 @@ puts "Account #{account_id} balance: $#{account.balance}"
 
 MessageStore::Postgres::Read.(command_stream_name) do |message_data|
   Handlers::Commands.(message_data)
+end
+
+puts "Handling transaction commands"
+
+MessageStore::Postgres::Read.(transaction_stream_name) do |message_data|
+  Handlers::Commands::Transactions.(message_data)
 end
 
 

test/automated/handle_commands/deposit/not_reserved.rb
@@ -1,0 +1,23 @@
+require_relative '../../../test_init'
+
+context "Handle Commands" do
+  context "Deposit" do
+    context "Not Reserved" do
+      deposit_id = Identifier::UUID::Random.get
+
+      deposit = Controls::Commands::Deposit.example(id: deposit_id)
+
+      transaction_stream_name = "accountTransaction-#{deposit_id}"
+
+      Messaging::Postgres::Write.(deposit, transaction_stream_name)
+
+      Handlers::Commands.(deposit)
+
+      event_data, * = MessageStore::Postgres::Get.(transaction_stream_name, position: 1, batch_size: 1)
+
+      test "Deposit command is not written to transaction stream" do
+        assert(event_data.nil?)
+      end
+    end
+  end
+end

test/automated/handle_commands/deposit/reserved.rb
@@ -1,0 +1,41 @@
+require_relative '../../../test_init'
+
+context "Handle Commands" do
+  context "Deposit" do
+    context "Reserved" do
+      deposit_id = Identifier::UUID::Random.get
+
+      deposit = Controls::Commands::Deposit.example(id: deposit_id)
+
+      Handlers::Commands.(deposit)
+
+      transaction_stream_name = "accountTransaction-#{deposit_id}"
+
+      event_data, * = MessageStore::Postgres::Get.(transaction_stream_name, position: 0, batch_size: 1)
+
+      written_message = Messaging::Message::Import.(event_data, deposit.class)
+
+      test "Deposit command is written to transaction stream" do
+        refute(written_message.nil?)
+      end
+
+      context "Attributes" do
+        test "deposit_id" do
+          assert(written_message.deposit_id == deposit.deposit_id)
+        end
+
+        test "account_id" do
+          assert(written_message.account_id == deposit.account_id)
+        end
+
+        test "amount" do
+          assert(written_message.amount == deposit.amount)
+        end
+
+        test "time" do
+          assert(written_message.time == deposit.time)
+        end
+      end
+    end
+  end
+end