Senjai/account-component

[STEP] Withdrawal reservation

exercise/handle_reissued_withdraw.rb
@@ -1,0 +1,52 @@
+require_relative './exercise_init'
+
+account_id = Identifier::UUID::Random.get
+withdrawal_id = Identifier::UUID::Random.get
+
+
+deposited = Messages::Events::Deposited.new
+deposited.deposit_id = Identifier::UUID::Random.get
+deposited.account_id = account_id
+deposited.amount = 100
+deposited.time = '2000-01-01T11:11:11.000Z'
+deposited.processed_time = '2000-01-01T22:22:22.000Z'
+deposited.transaction_position = 0
+
+Messaging::Postgres::Write.(deposited, "account-#{account_id}")
+
+
+withdraw = Messages::Commands::Withdraw.new
+withdraw.withdrawal_id = withdrawal_id
+withdraw.account_id = account_id
+withdraw.amount = 11
+withdraw.time = '2000-01-01T11:11:11.000Z'
+
+command_stream_name = "account:command-#{account_id}"
+transaction_stream_name = "accountTransaction-#{withdrawal_id}"
+
+
+store = Store.build
+
+
+2.times do
+  Messaging::Postgres::Write.(withdraw, command_stream_name)
+end
+
+
+account = store.fetch(account_id)
+puts "Account #{account_id} balance: $#{account.balance}"
+
+
+EventSource::Postgres::Read.(command_stream_name) do |event_data|
+  Handlers::Commands.(event_data)
+end
+
+puts "Handling transaction commands"
+
+EventSource::Postgres::Read.(transaction_stream_name) do |event_data|
+  Handlers::Commands::Transactions.(event_data)
+end
+
+
+account = store.fetch(account_id)
+puts "Account #{account_id} balance: $#{account.balance}"

test/automated/handle_commands/withdraw/not_reserved.rb
@@ -1,0 +1,23 @@
+require_relative '../../../test_init'
+
+context "Handle Commands" do
+  context "Withdraw" do
+    context "Not Reserved" do
+      withdraw_id = Identifier::UUID::Random.get
+
+      withdraw = Controls::Commands::Withdraw.example(id: withdraw_id)
+
+      transaction_stream_name = "accountTransaction-#{withdraw_id}"
+
+      Messaging::Postgres::Write.(withdraw, transaction_stream_name)
+
+      Handlers::Commands.(withdraw)
+
+      event_data, * = EventSource::Postgres::Get.(transaction_stream_name, position: 1, batch_size: 1)
+
+      test "Withdraw command is not written to transaction stream" do
+        assert(event_data.nil?)
+      end
+    end
+  end
+end

test/automated/handle_commands/withdraw/reserved.rb
@@ -1,0 +1,41 @@
+require_relative '../../../test_init'
+
+context "Handle Commands" do
+  context "Withdraw" do
+    context "Reserved" do
+      withdraw_id = Identifier::UUID::Random.get
+
+      withdraw = Controls::Commands::Withdraw.example(id: withdraw_id)
+
+      Handlers::Commands.(withdraw)
+
+      transaction_stream_name = "accountTransaction-#{withdraw_id}"
+
+      event_data, * = EventSource::Postgres::Get.(transaction_stream_name, position: 0, batch_size: 1)
+
+      written_message = Messaging::Message::Import.(event_data, withdraw.class)
+
+      test "Withdraw command is written to transaction stream" do
+        refute(written_message.nil?)
+      end
+
+      context "Attributes" do
+        test "withdrawal_id" do
+          assert(written_message.withdrawal_id == withdraw.withdrawal_id)
+        end
+
+        test "account_id" do
+          assert(written_message.account_id == withdraw.account_id)
+        end
+
+        test "amount" do
+          assert(written_message.amount == withdraw.amount)
+        end
+
+        test "time" do
+          assert(written_message.time == withdraw.time)
+        end
+      end
+    end
+  end
+end

evt-archive/account-component-pre-sequence

exercise/handle_reissued_withdraw.rb
@@ -1,0 +1,52 @@
+require_relative './exercise_init'
+
+account_id = Identifier::UUID::Random.get
+withdrawal_id = Identifier::UUID::Random.get
+
+
+deposited = Messages::Events::Deposited.new
+deposited.deposit_id = Identifier::UUID::Random.get
+deposited.account_id = account_id
+deposited.amount = 100
+deposited.time = '2000-01-01T11:11:11.000Z'
+deposited.processed_time = '2000-01-01T22:22:22.000Z'
+deposited.transaction_position = 0
+
+Messaging::Postgres::Write.(deposited, "account-#{account_id}")
+
+
+withdraw = Messages::Commands::Withdraw.new
+withdraw.withdrawal_id = withdrawal_id
+withdraw.account_id = account_id
+withdraw.amount = 11
+withdraw.time = '2000-01-01T11:11:11.000Z'
+
+command_stream_name = "account:command-#{account_id}"
+transaction_stream_name = "accountTransaction-#{withdrawal_id}"
+
+
+store = Store.build
+
+
+2.times do
+  Messaging::Postgres::Write.(withdraw, command_stream_name)
+end
+
+
+account = store.fetch(account_id)
+puts "Account #{account_id} balance: $#{account.balance}"
+
+
+MessageStore::Postgres::Read.(command_stream_name) do |event_data|
+  Handlers::Commands.(event_data)
+end
+
+puts "Handling transaction commands"
+
+MessageStore::Postgres::Read.(transaction_stream_name) do |event_data|
+  Handlers::Commands::Transactions.(event_data)
+end
+
+
+account = store.fetch(account_id)
+puts "Account #{account_id} balance: $#{account.balance}"

test/automated/handle_commands/withdraw/not_reserved.rb
@@ -1,0 +1,23 @@
+require_relative '../../../test_init'
+
+context "Handle Commands" do
+  context "Withdraw" do
+    context "Not Reserved" do
+      withdraw_id = Identifier::UUID::Random.get
+
+      withdraw = Controls::Commands::Withdraw.example(id: withdraw_id)
+
+      transaction_stream_name = "accountTransaction-#{withdraw_id}"
+
+      Messaging::Postgres::Write.(withdraw, transaction_stream_name)
+
+      Handlers::Commands.(withdraw)
+
+      event_data, * = MessageStore::Postgres::Get.(transaction_stream_name, position: 1, batch_size: 1)
+
+      test "Withdraw command is not written to transaction stream" do
+        assert(event_data.nil?)
+      end
+    end
+  end
+end

test/automated/handle_commands/withdraw/reserved.rb
@@ -1,0 +1,41 @@
+require_relative '../../../test_init'
+
+context "Handle Commands" do
+  context "Withdraw" do
+    context "Reserved" do
+      withdraw_id = Identifier::UUID::Random.get
+
+      withdraw = Controls::Commands::Withdraw.example(id: withdraw_id)
+
+      Handlers::Commands.(withdraw)
+
+      transaction_stream_name = "accountTransaction-#{withdraw_id}"
+
+      event_data, * = MessageStore::Postgres::Get.(transaction_stream_name, position: 0, batch_size: 1)
+
+      written_message = Messaging::Message::Import.(event_data, withdraw.class)
+
+      test "Withdraw command is written to transaction stream" do
+        refute(written_message.nil?)
+      end
+
+      context "Attributes" do
+        test "withdrawal_id" do
+          assert(written_message.withdrawal_id == withdraw.withdrawal_id)
+        end
+
+        test "account_id" do
+          assert(written_message.account_id == withdraw.account_id)
+        end
+
+        test "amount" do
+          assert(written_message.amount == withdraw.amount)
+        end
+
+        test "time" do
+          assert(written_message.time == withdraw.time)
+        end
+      end
+    end
+  end
+end